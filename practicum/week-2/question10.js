1d09f900969db7b58307fa7b3e8734dd1e3fe386
tolain <90009568+tolain@users.noreply.github.com>
2021-09-10 19:24:25 -0700

    Update question10.js

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,13 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    let maximum = 0;
+    let minimum = 9;
+    while (n!==0) {
+        let r = n % 10;
+        maximum = Math.max(r, maximum);
+        minimum = Math.min(r, minimum);
+        n = parseInt(n / 10);  
+    }
+        return maximum-minimum;
 };
