14f24c256f1474d13f6794e8ae8c96bfb776d767
tolain <90009568+tolain@users.noreply.github.com>
2021-09-14 21:52:20 -0700

    Update question11.js

--- practicum/week-3/question11.js
+++ practicum/week-3/question11.js
@@ -14,5 +14,17 @@ output: 1
 */
 
 var d_count = function(input1, input2, d) {
-
-};
+    let count = 0;
+    for (let i = 0; i < input1.length; i++) {
+        let found = false;
+        for (let j = 0; j < input2.length; j++) {
+            if (Math.abs(input1[i] - input2[j]) <= d) {
+                found = true;
+            }
+        }
+        if (!found) {
+            ++count;
+        }
+    }
+    return count;
+}
