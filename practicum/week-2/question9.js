b28e24a4c5edd06f218be8c6a9bfba5a82466952
tolain <90009568+tolain@users.noreply.github.com>
2021-12-12 20:01:46 -0800

    Update question9.js

--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -9,16 +9,12 @@ output: 'hello'
 */
 
 var suffleString = function(input, shuffleIndices) {
-<<<<<<< HEAD
-  const res = [];
-=======
-  var res = [];
->>>>>>> 42b1682d505cbc2dcf13ccad234df560baa332d4
-  for (let i = 0; i < shuffleIndices.length; i++){
-    let result = shuffleIndices[i];
-    res[result] = input.charAt(i);
-  }
-  return res.join("");
+  let s = new Array(shuffleIndices.length);
 
+    const inputSplit = input.split("");
+    inputSplit.forEach((letter, index) => {
+        s[shuffleIndices[index]] = letter;
+    });
+
+    return s.join("");
 };
-console.log(suffleString('emelwco', [1,5,6,2,0,3,4]))
